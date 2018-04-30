b828560a054a6b3e0748ac9d41df0994e29d7283
J3ffcon1 <Jrlone1990@gmail.com>
2018-04-30 14:01:51 -0700

    artists post route method passing

--- test/e2e/artists.test.js
+++ test/e2e/artists.test.js
@@ -2,7 +2,7 @@ const { assert } = require('chai');
 const request = require('./request');
 const { dropCollection } = require('./db');
 
-describe('Artist E2E Test', () =>{
+describe.only('Artist E2E Test', () =>{
     before(() => dropCollection('albums'));
     before(() => dropCollection('artists'));
 
@@ -21,19 +21,36 @@ describe('Artist E2E Test', () =>{
 
     let artist1 = {
         name:'Kate Bush',
+        albums:[],
         genre: 'Alternative'
     };
 
-    let artist2 = {
-        name:'Grimes',
-        genre: 'Pop'
-    };
-
-    // const checkOk = res => {
-    //     if(!res.ok) throw res.error;
-    //     return res;
+    // let artist2 = {
+    //     name:'Grimes',
+    //     genre: 'Pop'
     // };
 
+    const checkOk = res => {
+        if(!res.ok) throw res.error;
+        return res;
+    };
+
+    it('posts an artist to the db', () => {
+        return request.post('/artists')
+            .send(artist1)
+            .then(checkOk)
+            .then(({ body }) => {
+                const { _id, __v, genre, albums } = body;
+                assert.ok(_id);
+                assert.equal(__v, 0);
+                assert.ok(genre);
+                assert.deepEqual(body, {
+                    ...artist1,
+                    _id, __v, genre, albums
+                });
+                artist1 = body;
+            });
+    });
 
 
 
