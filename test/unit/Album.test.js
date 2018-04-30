0d7fbc7dda6ac5219abaaba9d5df2d023b89d4c7
Steele Walston <steele.walston@gmail.com>
2018-04-30 10:34:57 -0700

    album model unit tests pass

---
+++ test/unit/Album.test.js
@@ -0,0 +1,29 @@
+const { assert } = require('chai');
+const { Types } = require('mongoose');
+const Album = require('../../lib/models/Album');
+const { getErrors } = require('./helpers');
+
+describe('Album model', () => {
+    
+    it('valid good model', () => {
+        const data = {
+            artist: Types.ObjectId(),
+            title: 'album1',
+            length: '1:00:2',
+            tracklist: [Types.ObjectId()]
+        };
+
+        const album = new Album(data);
+        data._id = album._id;
+        assert.deepEqual(album.toJSON(), data);
+        assert.isUndefined(album.validateSync());
+    });
+
+    it('required fields', () => {
+        const album = new Album({});
+        const errors = getErrors(album.validateSync(), 2);
+        assert.equal(errors.title.kind, 'required');
+        assert.equal(errors.length.kind, 'required');
+    });
+
+});
