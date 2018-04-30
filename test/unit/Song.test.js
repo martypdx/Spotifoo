7ce99c7edcb4332956f7d4c194f75b0509455955
Steele Walston <steele.walston@gmail.com>
2018-04-30 10:18:21 -0700

    song model unit test passes

---
+++ test/unit/Song.test.js
@@ -0,0 +1,30 @@
+const { assert } = require('chai');
+const { Types } = require('mongoose');
+const Song = require('../../lib/models/Song');
+const { getErrors } = require('./helpers');
+
+describe('Song model', () => {
+    
+    it('valid good model', () => {
+        const data = {
+            title: 'song1',
+            artist: Types.ObjectId(),
+            length: '2:02',
+            album: Types.ObjectId(),
+            playcount: 3
+        };
+
+        const song = new Song(data);
+        data._id = song._id;
+        assert.deepEqual(song.toJSON(), data);
+        assert.isUndefined(song.validateSync());
+    });
+
+    it('required fields', () => {
+        const song = new Song({});
+        const errors = getErrors(song.validateSync(), 2);
+        assert.equal(errors.title.kind, 'required');
+        assert.equal(errors.length.kind, 'required');
+    });
+
+});
