50f948f42a440a8ae1280e4447a4060ade0fc887
J3ffcon1 <Jrlone1990@gmail.com>
2018-05-03 09:53:01 -0700

    created playlist aggregation test

---
+++ test/e2e/playlists.aggregation.test.js
@@ -0,0 +1,12 @@
+const { assert } = require('chai');
+const request = require('./request');
+const { dropCollection } = require('./db');
+const { verify } = require('../../lib/util/token-service');
+
+describe('Playlist Aggregation', () => {
+
+
+
+
+
+});
