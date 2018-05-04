4d75e55768c0990b2ebed73805a5274d60731348
Steele Walston <steele.walston@gmail.com>
2018-05-04 12:28:55 -0700

    URI stuff

--- test/e2e/db.js
+++ test/e2e/db.js
@@ -3,7 +3,7 @@ const connect = require('../../lib/util/connect');
 const mongoose = require('mongoose');
 const request = require('./request');
 
-before(() => connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/spotifoo'));    
+before(() => connect(process.env.MONGODB_URI));    
 after(() => mongoose.connection.close());
 
 module.exports = {
